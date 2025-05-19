0e3e07ca273bf6c0855d6328ae4c3d0b2f9428c8
skypi11 <mattmolines@gmail.com>
2025-05-19 12:27:12 +0200

    Edit
    Edit JS add addevenlistener for all button

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-const pad = document.querySelectorAll('button')
-console.log(pad)
-const value = document.querySelector('input')
-console.log(value.value)
-for (const element of pad){
-   // console.log(element.textContent)
-    element.addEventListener('click',() =>{
-        
-        console.log(element)
-        value.value =element.textContent
-    })
-    
-
+const pad = document.querySelectorAll("button");
+console.log(pad);
+const screenShoot = document.querySelector("input");
+const clear = document.querySelector("clear");
+const a = 
+const b = 
+const c =
+for (const element of pad) {
+  // console.log(element.textContent)
+  element.addEventListener("click", () => {
+    console.log(element);
+    screenShoot.value += element.textContent;
+    if (screenShoot.value.includes("C")) {
+      screenShoot.value = "";
+    }
+  });
 }
